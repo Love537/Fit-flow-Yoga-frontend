3a5b71240365f47a0d1927b227b173d06ccca29e
Love537 <lovepreetsingh537@gmail.com>
2025-06-01 20:26:14 +1200

    first commit

---
+++ src/config/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4Hu1XhU42GkCy2fAm-3TBFQmIIttOdHw",
+  authDomain: "yoga-fit-flow.firebaseapp.com",
+  projectId: "yoga-fit-flow",
+  storageBucket: "yoga-fit-flow.firebasestorage.app",
+  messagingSenderId: "832600066963",
+  appId: "1:832600066963:web:34ecd3f616cb273235b666"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
